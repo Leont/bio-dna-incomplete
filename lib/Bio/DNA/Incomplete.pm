a86be44b3850b3eefe2aa68635bc94bc8e257678
Leon Timmermans <fawaka@gmail.com>
2011-10-19 15:56:58 +0200

    Added all_possibilities, improved case-insensitivity

--- lib/Bio/DNA/Incomplete.pm
+++ lib/Bio/DNA/Incomplete.pm
@@ -2,9 +2,12 @@ package Bio::DNA::Incomplete;
 use strict;
 use warnings;
 
-use Sub::Exporter -setup => { exports => [qw/pattern_to_regex pattern_to_regex_string match_pattern/], groups => { default => [qw/pattern_to_regex pattern_to_regex_string match_pattern/]} };
+use Carp 'croak';
+use Sub::Exporter -setup => { exports => [qw/pattern_to_regex pattern_to_regex_string match_pattern all_possibilities/], groups => { default => [qw/pattern_to_regex pattern_to_regex_string match_pattern all_possibilities/]} };
 
-my %replacement = (
+my %simple = map { ( $_ => $_ ) } qw/A C G T/;
+
+my %meaning_of = (
 	R => 'AG',
 	Y => 'CT',
 	W => 'AT',
@@ -17,48 +20,76 @@ my %replacement = (
 	D => 'AGT',
 	N => 'ACGT',
 );
-$_ = "[$_]" for values %replacement;
-my $valid = map { qr/[^$_]/ } qw/A C G T/, keys %replacement;
+my %pattern_for = %meaning_of;
+$_ = "[$_]" for values %pattern_for;
+my ($invalid) = map { qr/[^$_]/ } join '', keys %simple, keys %pattern_for;
+my %bases_for = (%meaning_of, %simple);
+$_ = [ split // ] for values %bases_for;
 
 sub pattern_to_regex_string {
-	my $pattern = shift;
-	die 'Invalid pattern' if $pattern =~ /$valid/;
+	my $pattern = uc shift;
+	croak 'Invalid pattern' if $pattern =~ /$invalid/;
 
-	$pattern =~ s/([^ATCG])/$replacement{$1}/gi;
-	return qr/$pattern/
+	$pattern =~ s/([^ATCG])/$pattern_for{$1}/g;
+	return "(?i:$pattern)";
 }
 
 sub pattern_to_regex {
-	my $pattern = shift;
+	my $pattern = uc shift;
 	my $string = pattern_to_regex_string($pattern);
 	return qr/$string/;
 };
 
 sub match_pattern {
-    my $pattern = shift;
+	my ($pattern, @args) = @_;
     my $regex = pattern_to_regex($pattern);
-    local $_;
-    return grep { $_ =~ /\A $regex \z/xms } @_;
+    return grep { $_ =~ /\A $regex \z/xms } @args;
 }
- 
+
+sub _all_possibilities {
+	my ($current, @rest) = @_;
+	if (@rest) {
+		my @ret;
+		my $pretail = _all_possibilities(@rest);
+		for my $head (@{$bases_for{$current}}) {
+			for my $tail (@{$pretail}) {
+				push @ret, $head.$tail;
+			}
+		}
+		return \@ret;
+	}
+	else {
+		return $bases_for{$current};
+	}
+}
+
+sub all_possibilities {
+    my $pattern = uc shift;
+	my @bases = split //, $pattern;
+	return @{ _all_possibilities(@bases) };
+}
 
 1;
 
-__END__
-
 #ABSTRACT: Match incompletely specified bases in nucleic acid sequences
+
+__END__
 
 =func match_pattern($pattern, @things_to_test)
 
 Returns the list of sequences that match C<$pattern>.
 
-=func pattern_to_regex
+=func pattern_to_regex($pattern)
 
 Returns a compiled regex which is the equivalent of the pattern.
 
-=func pattern_to_regex_string
+=func pattern_to_regex_string($pattern)
 
-Returns a regex string which is the equivalent of the globbing pattern.
+Returns a regex string which is the equivalent of the pattern.
+
+=func all_possibilities($pattern)
+
+Returns a list of all possible sequences that can match the pattern.
 
 =head1 SEE ALSO
 
--- t/10-basics.t
+++ t/10-basics.t
@@ -4,12 +4,17 @@ use strict;
 use warnings;
 
 use Test::More 0.88;
+use Test::Exception;
 
 use Bio::DNA::Incomplete -all;
 
-ok(match_pattern("ACGT", "ACGT"));
-ok(match_pattern("ACKT", "ACGT"));
-ok(!match_pattern("KCKT", "ACGT"));
-ok(!match_pattern("ACKT", "ACGT "));
+ok(match_pattern('ACGT', 'ACGT'),"'ACGT' matches 'ACGT'");
+ok(match_pattern('ACKT', 'ACGT'), "'ACKT' matches 'ACGT'");
+ok(match_pattern('ACKT', 'acgt'), "'ACKT' matches 'acgt'");
+ok(match_pattern('ackt', 'ACGT'), "'ackt' matches 'ACGT'");
+ok(!match_pattern('KCKT', 'ACGT'), "'KCKT' does not match 'ACGT'");
+ok(!match_pattern('ACKT', 'ACGT '), "'ACKT ' does not match 'ACGT'");
+
+throws_ok { pattern_to_regex_string("XXX") } qr/Invalid/, 'Invalid pattern gives an error';
 
 done_testing;
